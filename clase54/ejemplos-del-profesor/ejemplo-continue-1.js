056ad2d04aa7dd2bea310e8f8d32e0af4491dcfa
Alex <alexgonzalezariasdesaavedra@hotmail.com>
2026-06-02 20:02:31 +0200

    Borrada la carpeta de ejemplos del profesor

--- clase54/ejemplos-del-profesor/ejemplo-continue-1.js
+++
@@ -1,9 +0,0 @@
-let impares = []
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 === 0) { // Si el número es par...
-        continue;      // ...salta a la siguiente iteración
-    }
-    console.log(`Procesando número impar: ${i}`);
-  impares.push(i)
-}
-console.log(impares)
